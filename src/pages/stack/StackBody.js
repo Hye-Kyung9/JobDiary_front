19fe94995de4088fdfc1041cc86d8219a94d77ac
Hye-Kyung9 <rngprud123@naver.com>
2022-06-04 01:23:54 +0900

    ADD StackCard

--- src/pages/stack/StackBody.js
+++ src/pages/stack/StackBody.js
@@ -1,55 +1,72 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
 
-const styles = (theme) => ({
-  root: {
-    padding: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(3),
-    textAlign: "center",
-    color: theme.palette.text.primary,
-  },
-});
+const StackBody = () => {
+  return (
+    <div>
+      <Row xs={1} md={2} className="g-4">
+        {/* {Array.from({ length: 9 }).map((_, idx) => ( */}
+        <Col>
+          <Card>
+            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            <img
+              src="https://static.codenary.co.kr/company_logo/1.png"
+              alt=""
+              className="logo_img"
+            ></img>
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        {/* ))} */}
+      </Row>
+    </div>
+  );
+};
 
-class StackBody extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>
-              Content 1Content 1Content 1Content 1Content 1Content 1Content
-              1Content 1Content 1
-            </Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>살려주세여</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>진짜ㅏ악</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>이것도 그냥 가져온거긴 한데</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>Content 5</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>Content 6</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>Content 7</Paper>
-          </Grid>
-          <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>Content 8</Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
-}
-export default withStyles(styles)(StackBody);
+export default StackBody;
